f642a28997e64fbadf44ad09ba36ed14369937c3
shouvik <shouvikdeebee@gmail.com>
2024-02-25 15:32:31 -0500

    forgot to save my change oops

--- backend/storeManager/models.py
+++ backend/storeManager/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    position = models.CharField(max_length=50)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
