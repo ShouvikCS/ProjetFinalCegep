4cbc72ffef95e9db06b7328428097f9e80905d95
shouvik <shouvikdeebee@gmail.com>
2024-03-14 23:05:01 -0400

    command line fix

--- backend/storeManager/models.py
+++ backend/storeManager/models.py
@@ -28,3 +28,6 @@ class CommandLine(models.Model):
     command = models.ForeignKey(Command, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"Command Line: {self.product.name} - Quantity: {self.quantity}"
